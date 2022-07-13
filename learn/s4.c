6d3a7d2e8845e3b0c125859c6d6d51303c829979
kumarsurajstm <109150650+kumarsurajstm@users.noreply.github.com>
2022-07-13 16:45:53 +0530

    Add files via upload

---
+++ learn/s4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+float r,area;    
+printf("enter radius of circle");
+scanf("%f",&r);
+area=3.14*r*r;
+printf("area of circle is %f having the radius",area);
+return 0;
+
+
+}
---
+++ learn/s9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+
+int x=printf("ineuroin");
+printf("%d",x);
+return 0;
+
+}
